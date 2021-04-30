f33ff3620716a26ee9ba0339eda1ab460d3d7674
fixmol <fixmol@mail.ru>
2021-04-30 23:37:29 +0300

    The project is cleared for rewriting

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <header class="navbar">
-    <p>Freelance Service</p>
-    <ul class="navbar-menu">
-      <li><router-link to="/tasklist">Все задачи</router-link></li>
-      <li><router-link to="/tasknew">Создать задачу</router-link></li>
-    </ul>
-  </header>
-
-  <div class="container">
-    <router-view></router-view>
+  <the-navbar></the-navbar>
+  <div class="container with-nav">
+    <router-view />
   </div>
 </template>
 
 
 <script>
+import TheNavbar from './components/TheNavbar'
+
 export default {
-  
+  components: {
+    TheNavbar
+  }
 }
 </script>
 
 
 <style>
-
 </style>
